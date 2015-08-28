e9dcddc9332bfad6d5802d5e365d5193b4770dc7
historypeats <iam.historypeats@gmail.com>
2015-08-28 15:49:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Rebear
+
+The hunter has become the hunted. This is a honeypot that takes a picture of the person touching your Macbook, saves it, then enables the Screensaver (which should require a password to recover from).
+
+
+
+## Installation
+1. Download/clone this project
+2. ```cd rebear```
+3. Download imagesnap (http://iharder.sourceforge.net/current/macosx/imagesnap/)
+4. Extract the imagesnap binary and place is in the rebear directory
+5. ```chmod +x rebear.py```
+6. Enable Assistive Devices and add your Terminal: https://support.apple.com/en-us/HT202866
+
+## Usage
+```
+user@computer$ ./rebear.py
+ ______     ______     ______     ______     ______     ______
+/\  == \   /\  ___\   /\  == \   /\  ___\   /\  __ \   /\  == \
+\ \  __<   \ \  __\   \ \  __<   \ \  __\   \ \  __ \  \ \  __<
+ \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\  \ \_\ \_\
+  \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_/\/_/   \/_/ /_/
+
+Author: historypeats
+
+[+] Starting rebear
+```
---
+++ rebear.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2.6
+import os
+import sys
+from datetime import datetime
+from subprocess import call
+from Cocoa import *
+from Foundation import *
+from PyObjCTools import AppHelper
+
+
+# Commands
+imagesnap = '%s/imagesnap' % os.getcwd()
+screenaver = '/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app'
+screensaver_action = 'open'
+
+
+class AppDelegate(NSObject):
+	def applicationDidFinishLaunching_(self, aNotification):
+		NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSKeyDownMask, handler)
+
+
+def get_filename():
+	'''Return a filename with datetime format'''
+	return datetime.now().isoformat() + '.jpg'
+
+
+def handler(event):
+	'''Event handler'''
+	try:
+		filename = get_filename()
+		print '[+] GOTEM!'
+		print '[+] Captured to file: %s' % filename
+
+		# Take webcam pic
+		call([imagesnap, '-q', filename])
+
+		# Activate screensaver
+		call([screensaver_action, screenaver])
+
+		# Closeout
+		AppHelper.stopEventLoop()
+	except KeyboardInterrupt:
+		AppHelper.stopEventLoop()
+
+
+def banner():
+	banner = ''' _____     ______     ______     ______     ______     ______    
+/\  == \   /\  ___\   /\  == \   /\  ___\   /\  __ \   /\  == \   
+\ \  __<   \ \  __\   \ \  __<   \ \  __\   \ \  __ \  \ \  __<   
+ \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\  \ \_\ \_\ 
+  \/_/ /_/   \/_____/   \/_____/   \/_____/   \/_/\/_/   \/_/ /_/ 
+                                                                  
+Author: historypeats
+	'''
+	return banner
+
+
+def main():
+	''' Main '''
+	print banner()
+	print '[+] Starting rebear'
+
+	# Init Global Event Listener
+	app = NSApplication.sharedApplication()
+	delegate = AppDelegate.alloc().init()
+	NSApp().setDelegate_(delegate)
+	AppHelper.runEventLoop()
+
+
+if __name__ == '__main__':
+	main()
